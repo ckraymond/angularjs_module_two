a0417cf519bf58a1f7aaef33e49ba12ba9185baa
Colin <craymond@Colins-MacBook-Pro.local>
2016-11-03 22:58:14 -0400

    Initial Commit
    Added in the base files and the structure.

---
+++ js/app.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+
+  ToBuyController.$inject = ['$scope'];
+  AlreadyBoughtController.$inject = ['$scope'];
+
+  function ToBuyController($scope) {
+
+
+  };
+
+  function AlreadyBoughtController($scope) {
+
+
+  };
+})();
