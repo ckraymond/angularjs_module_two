c49e20b957276cd03c1dc8122d11135127a0b0e1
Colin <craymond@Colins-MacBook-Pro.local>
2016-11-05 23:45:16 -0400

    Finished!

--- js/app.js
+++ js/app.js
@@ -3,35 +3,73 @@
 
   angular.module('ShoppingListCheckOff', [])
   .controller('ToBuyController', ToBuyController)
-  .controller('AlreadyBoughtController',AlreadyBoughtController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
   .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
 
 
   ToBuyController.$inject = ['ShoppingListCheckOffService'];
-  function ToBuyController($scope) {
+  function ToBuyController(ShoppingListCheckOffService) {
+    var showToBuy = this;
 
+    showToBuy.items_tobuy = ShoppingListCheckOffService.getToBuy();
 
+    showToBuy.moveToBought = function (itemIndex){
+      ShoppingListCheckOffService.moveToBought(itemIndex);
+      if (showToBuy.items_tobuy.length == 0) {
+        showToBuy.toBuyEmpty = ShoppingListCheckOffService.getBuyEmpty();
+      };
+    };
   };
+
 
   AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-  function AlreadyBoughtController($scope) {
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var showBought = this;
 
-
+    showBought.items_bought = ShoppingListCheckOffService.getBought();
+    showBought.boughtEmpty = function() {
+      if (showBought.items_bought.length == 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   };
 
-  ShoppingListCheckOffService.$inject = [''];
+
   function ShoppingListCheckOffService(){
     var service = this;
 
     // create two seperate arrays with items to buy or bought
     var items_tobuy = [{name: "monkeys", quantity: 10},
                        {name: "hats", quantity: 10},
-                       {name: "bells", quantity: 20}
-                       {name: "beer", quantity: 200}
-                       {name: "television", quantity: 1}
-                       {name: "speakers", quantity: 2}];
+                       {name: "bells", quantity: 20},
+                       {name: "beer", quantity: 20},
+                       {name: "television", quantity: 1},
+                       {name: "speakers", quantity: 2},
+                       {name: "Banana Chips", quantity: 1000}];
     var items_bought = [];
 
+    service.getToBuy = function (){
+      return items_tobuy;
+    };
+
+    service.getBought = function (){
+      return items_bought;
+    };
+
+    service.getBuyEmpty = function(){
+      if (items_tobuy.length == 0){
+        return true;
+      } else {
+        return false;
+      };
+    };
+
+    service.moveToBought = function(itemIdex){
+      items_bought.push(items_tobuy[itemIdex]);
+      items_tobuy.splice(itemIdex,1);
+    };
   };
 
 })();
