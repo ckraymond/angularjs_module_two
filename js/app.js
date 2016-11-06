3b818b598dd0823cf054339908261b1c2822b9b1
Colin <craymond@Colins-MacBook-Pro.local>
2016-11-05 21:52:52 -0400

    Change II

--- js/app.js
+++ js/app.js
@@ -3,17 +3,35 @@
 
   angular.module('ShoppingListCheckOff', [])
   .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController',AlreadyBoughtController)
+  .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
 
-  ToBuyController.$inject = ['$scope'];
-  AlreadyBoughtController.$inject = ['$scope'];
 
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
   function ToBuyController($scope) {
 
 
   };
 
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
   function AlreadyBoughtController($scope) {
 
 
   };
+
+  ShoppingListCheckOffService.$inject = [''];
+  function ShoppingListCheckOffService(){
+    var service = this;
+
+    // create two seperate arrays with items to buy or bought
+    var items_tobuy = [{name: "monkeys", quantity: 10},
+                       {name: "hats", quantity: 10},
+                       {name: "bells", quantity: 20}
+                       {name: "beer", quantity: 200}
+                       {name: "television", quantity: 1}
+                       {name: "speakers", quantity: 2}];
+    var items_bought = [];
+
+  };
+
 })();
